6ca628b832f16e49fd4f937f0883e04ad5abc1a5
Audrey Cooper <audrey_cooper@me.com>
2020-08-26 13:40:06 -0700

    add automatic moves

--- client.js
+++ client.js
@@ -11,7 +11,14 @@ const connect = function () {
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server')
-    conn.write('Name: ACO')
+    conn.write('Name: ACO');
+
+    // conn.write('Move: up');
+    // conn.write('Move: up');
+    // const upInterval = setInterval(() => conn.write('Move: up'), 100)
+    // setTimeout(() => clearInterval(upInterval), 25000)
+    // const rightInterval = setInterval(() => conn.write('Move: right'), 1000)
+    // setTimeout(() => clearInterval(rightInterval), 5000)
   });
 
   conn.on('data', (data) => {
